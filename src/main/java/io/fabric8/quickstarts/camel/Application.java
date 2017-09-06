60558c5b5cfdc896005b41d20c17d4c391b9e307
jwebber77 <jon.webber@dsl.pipex.com>
2017-09-06 16:28:12 +0100

    test

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -35,7 +35,7 @@ public class Application extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("timer://foo?period=5000")
-            .setBody().constant("Hello World Jon's Test")
+            .setBody().constant("Hello World Jon's Test ")
             .log(">>> ${body}");
     }
 }
