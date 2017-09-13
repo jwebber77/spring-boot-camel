becb8fb2a1687601d6792850b09f615d3865af3c
jwebber77 <jon.webber@dsl.pipex.com>
2017-09-13 11:13:03 +0100

    updated

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -29,7 +29,8 @@ public class Application extends RouteBuilder {
 
     // must have a main method spring-boot can run
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.out.println("fuck off!");
+    	SpringApplication.run(Application.class, args);
     }
 
     @Override
